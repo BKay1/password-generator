98a08fad19ed2b43aa8cad7dc3b152e9807fa14c
Bee Kay <beekayuni@gmail.com>
2021-03-16 12:40:20 +0000

    added array characters and first user prompt for password length

--- Develop/script.js
+++ Develop/script.js
@@ -9,23 +9,32 @@ var generateBtn = document.querySelector("#generate");
 
 const lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-const specialcharArray = ["#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","["\""\ "\'"]
-const numbercharArray = ["0","1","2","3","4","5","6","7","8","9"];
+const specialcharArray = ["#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@"];
+const numbercharArray = ["0","1","2","3","4","5","6","7","8","9",];
 
+//["\""\ "\'" - recheck!!
 
 //Prompt user for passwordLength
 //if  less than <8 characters in length or more than >128 characters. Alert user to put in correct passwordLength.
 
 
+const passwordLength = prompt("How many characters would you like your password to contain?");
+
+  if (!passwordLength) {
+      alert("Please add how many characters you would like your password to contain");
+    }
+    if (passwordLength < 8) || (passwordLength > 128) {
+        alert("Your password must be between 8-128 characters");
+   }
+
+  
+//Prompt and confirm with user re password characters
+const confirmLowercase = confirm("Do you want lowercase characters in your password")
+const confirmUppercase = confirm("Do you want lowercase characters in your password")
+const confirmSpecialChar = confirm("Do you want special characters in your password")
+const confirmNumber = confirm("Do you want number characters in your password")
 
 
-//Prompt user for lowercase characters
-//Prompt user for uppercase characters
-//Prompt user for special characters
-//Prompt user for numeric characters
-
-
-//Confirm user inputs for characters
 
 //Generate Password
 
