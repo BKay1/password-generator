3a6376d7cda17b42e6c5225eb62350f2840f2c65
Bee Kay <beekayuni@gmail.com>
2021-03-16 11:14:18 +0000

    Declared arrays for lowercase, uppercase, specialchar and numbers

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,10 @@ var generateBtn = document.querySelector("#generate");
 
 //Declare arrays here for lowercase, uppercase, special characters and numbers.
 
+const lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const specialcharArray = ["#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","["\""\ "\'"]
+const numbercharArray = ["0","1","2","3","4","5","6","7","8","9"];
 
 
 //Prompt user for passwordLength
